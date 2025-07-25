1f0494d4566c7dc357779c94e549b4bdd7051a43
Jakub Brylak <brylakjakub@gmail.com>
2025-07-25 13:20:56 +0200

    Initial RockMusicStoreApplication sources and maven wrapper files

---
+++ backend/src/main/kotlin/com/jbs/rock_music_store/RockMusicStoreApplication.kt
@@ -0,0 +1,11 @@
+package com.jbs.rock_music_store
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RockMusicStoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<RockMusicStoreApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/jbs/rock_music_store/RockMusicStoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jbs.rock_music_store
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RockMusicStoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
